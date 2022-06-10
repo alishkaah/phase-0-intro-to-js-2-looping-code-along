79a1618d73c959af8f63edc3c6b35d3b60cf51bc
Moringa <alinoorshukri8@gmail.com>
2022-06-10 08:35:20 +0300

    commit tree 3e2101f83a7cb7abd8b66e258628126a90a36fd5

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(arr,event){
+    const newArray= [];
+    for (let i=0 ;i< arr.length ; i++){
+       // newArray[i] = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+       newArray.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+
+        console.log(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray
+}
+writeCards(["Guadalupe", "Ollie","Aki"], "surprise");
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
+countDown(10);
